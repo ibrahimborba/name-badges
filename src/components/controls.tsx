20c0b2af616ebdbe9ff695d7180ea47d22bbdbb9
Ibrahim <ibrahim.borba@gmail.com>
2024-06-18 06:25:41 -0300

    refactor: move controls from jsx to tsx

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,6 +1,4 @@
-import PropTypes from 'prop-types';
-
-const ControlPanel = ({ name, onChange }) => {
+const ControlPanel = ({ name, onChange }: { name: string, onChange: () => void}) => {
   return (
     <form
       className="flex flex-row gap-4 text-white bg-primary-10"
@@ -18,11 +16,6 @@ const ControlPanel = ({ name, onChange }) => {
       </div>
     </form>
   );
-};
-
-ControlPanel.propTypes = {
-  name: PropTypes.string,
-  onChange: PropTypes.func,
 };
 
 export default ControlPanel;
